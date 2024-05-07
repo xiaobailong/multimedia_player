f317f4d6c31c24dc7245bf42fee8d82bfaca25aa
guangbai <guangbai2023.!@>
2024-05-07 09:20:39 +0800

    完善打包方式

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 :: pyinstaller -F -w --hidden-import=loguru Player.py
 
-:: pyinstaller -D -w --hidden-import=loguru --contents-directory "libs" Player.py
+pyinstaller -D -w --hidden-import=loguru --contents-directory "libs" --add-data=libs/ffmpeg/ffmpeg.exe;ffmpeg Player.py
 
-pyinstaller --clean Player.spec
+::pyinstaller --clean Player.spec
