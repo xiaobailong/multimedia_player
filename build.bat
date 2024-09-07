fc4da8150098c4c1010dc13d2204e3a3523d6715
广白 <guangbai@netopstec.com>
2024-09-07 10:48:39 +0800

    优化剪切时间框填充数据逻辑

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 :: pyinstaller -F -w --hidden-import=loguru Player.py
 
-pyinstaller -D -w --hidden-import=loguru --contents-directory "libs" --add-data=libs/ffmpeg/ffmpeg.exe;ffmpeg Player.py
+rd /s/q dist
+pyinstaller -D -w --clean --hidden-import=loguru --contents-directory "libs" --add-data=libs/ffmpeg/ffmpeg.exe;ffmpeg Player.py
 
 ::pyinstaller --clean Player.spec
--- src/data_manager/sqlite3_client.py
+++ src/data_manager/sqlite3_client.py
@@ -1,40 +1,40 @@
 # -*- coding: utf-8 -*-
 
-import sqlite3
+import apsw
 from loguru import logger
+
 
 class Sqlite3Client:
 
     def __init__(self, *args, **kwargs):
-        self.conn = sqlite3.connect('player.db')
+        self.conn = apsw.Connection('player.db')
 
-    def showData(self,sql):
+    def showData(self, sql):
         c = self.conn.cursor()
         cursor = c.execute(sql)
         for row in cursor:
             logger.info(row)
 
-    def exeQuery(self,sql):
+    def exeQuery(self, sql):
         c = self.conn.cursor()
         cursor = c.execute(sql)
         return cursor
 
-    def exeUpdate(self,sql):
+    def exeUpdate(self, sql):
         c = self.conn.cursor()
         c.execute(sql)
-        self.conn.commit()
 
     def close(self):
         self.conn.close()
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     client = Sqlite3Client()
 
     # sqlCreateTable='create table people(id int primary key,name varcahr(10),address varcahr(50))'
-    sqlInsert0='insert into people values(1,"李宁","Shenyang")'
+    sqlInsert0 = 'insert into people values(1,"李宁","Shenyang")'
     sqlInsert1 = 'insert into people values(2,"超人","克星")'
-    sqlUpdate='UPDATE people set address = "shandong" where ID=1'
+    sqlUpdate = 'UPDATE people set address = "shandong" where ID=1'
     # selectSql="SELECT id, name, address  from people"
     # selectSql="SELECT id, name, address  from people where id = (SELECT max(id)  from people)"
     # selectSql = "SELECT *  from history"
